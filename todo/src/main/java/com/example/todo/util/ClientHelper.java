67805dc231464983ce0da05e8bba0623fd85a9a3
Jeff Currier <Jeff@Jeffs-MacBook-Pro.local>
2017-01-01 17:37:43 -0800

    remove dead code

--- todo/src/main/java/com/example/todo/util/ClientHelper.java
+++ todo/src/main/java/com/example/todo/util/ClientHelper.java
@@ -17,7 +17,7 @@ import com.mongodb.MongoClientURI;
  */
 public class ClientHelper {
     private final String MONGO_CONN_URI =
-            "mongodb://jcurrier:goodfoot@ds139968-a0.mlab.com:39968,ds139968-a1.mlab.com:39968/sample_db?replicaSet=rs-ds139968";
+            "mongodb://user:pw@ds139968-a0.mlab.com:39968,ds139968-a1.mlab.com:39968/sample_db?replicaSet=rs-ds139968";
     private static final String ROLE_ARN =
             "arn:aws:iam::099090753569:role/todo_svc_role";
     private static AWSCredentials m_creds = null;
@@ -44,42 +44,6 @@ public class ClientHelper {
     }
 
     public AmazonDynamoDBClient getDynamoClient() {
-
-        /*
-        AWSSecurityTokenServiceClient stsClient = new AWSSecurityTokenServiceClient();
-
-        AssumeRoleRequest assumeRequest = new AssumeRoleRequest()
-                .withRoleArn(ROLE_ARN)
-                .withRoleSessionName("todo");
-
-        AssumeRoleResult assumeResult =
-                stsClient.assumeRole(assumeRequest);
-
-        BasicSessionCredentials tmpCreds =
-                new BasicSessionCredentials(
-                        assumeResult.getCredentials().getAccessKeyId(),
-                        assumeResult.getCredentials().getSecretAccessKey(),
-                        assumeResult.getCredentials().getSessionToken());
-
-        AmazonDynamoDBClient dynamoClient = new AmazonDynamoDBClient(tmpCreds);
-        dynamoClient.setRegion(Region.getRegion(Regions.US_WEST_2));
-
-        // Step 2. AssumeRole returns temporary security credentials for
-        // the IAM role.
-
-        BasicSessionCredentials temporaryCredentials =
-                new BasicSessionCredentials(
-                        assumeResult.getCredentials().getAccessKeyId(),
-                        assumeResult.getCredentials().getSecretAccessKey(),
-                        assumeResult.getCredentials().getSessionToken());
-
-        // Step 3. Make DynamoDB service calls to read data from a
-        // DynamoDB table, stored in research@example.com, using the
-        // temporary security credentials from the DynamoDB-ReadOnly-role
-        // that were returned in the previous step.
-
-        AmazonDynamoDBClient dynamoClient = new AmazonDynamoDBClient(temporaryCredentials);
-        */
 
         AmazonDynamoDBClient client = new AmazonDynamoDBClient(new ProfileCredentialsProvider());
         client.setRegion(Region.getRegion(Regions.US_WEST_2));
